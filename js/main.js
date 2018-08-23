6d39cfcbe1e27fda6117c49bd27dd81da2276cbf
Emily Lam <emilylam1668@gmail.com>
2018-08-22 22:33:16 -0700

    lesson 4 (1/4)

---
+++ js/main.js
@@ -0,0 +1,17 @@
+func generateHTMLForBoardSquares () {
+  const numberOfSquares = 16;
+  let squaresHTML = '';
+
+  for (let i = 0; i < numberOfSquares; i++) {
+    squaresHTML += `
+      <div class="col-3 board-square">
+        <div class="face-container">
+          <div class="facedown"></div>
+          <div class="faceup"></div>
+        </div>
+      </div>`;
+  }
+
+  const boardElement = document.getElementById('gameboard');
+  boardElement.innerHTML = squaresHTML;
+}
