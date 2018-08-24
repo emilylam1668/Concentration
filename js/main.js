ee975c1bb6169add1f24bfb0d55e7e8df4787432
Emily Lam <emilylam1668@gmail.com>
2018-08-23 19:39:38 -0700

    lesson 4 (3/4)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+class BoardSquare {
+
+  constructor(element, color) {
+    this.element = element;
+    this.isFaceUp = false;
+    this.isMatched = false;
+    this.setColor(color);
+  }
+
+  setColor(color) {
+
+    const faceUpElement = this.element.getElementByClassName('faceup')[0];
+    this.color = color;
+    faceUpElement.classList.add(color);
+
+  }
+}
+
 func generateHTMLForBoardSquares () {
   const numberOfSquares = 16;
   let squaresHTML = '';
@@ -14,4 +32,40 @@ func generateHTMLForBoardSquares () {
 
   const boardElement = document.getElementById('gameboard');
   boardElement.innerHTML = squaresHTML;
+}
+
+const colorPairs = [];
+
+function generateColorPairs() {
+  if (colorPairs.length > 0) {
+    return colorPairs;
+  } else {
+    for (let i = 0; i < 8, i++) {
+      colorPairs.push('color-' + i);
+      colorPairs.push('color-' + i);
+    }
+    return colorPairs;
+  }
+}
+
+function shuffle(array) {
+  let currentIndex = array.length;
+  let temporaryValue, randomIndex;
+
+  //While there remains elements to shuffle
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+  //And swap with current element
+  temporaryValue = array[currentIndex];
+  array[currentIndex] = array[randomIndex];
+  array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+function shuffleColors() {
+  const colorPairs = generateColorPairs()
+  return shuffle(colorPairs);
 }
